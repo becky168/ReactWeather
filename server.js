f8b47396d07212e6bb640c96c89c78331d0bca37
Becky <yrstudio9316@gmail.com>
2017-01-06 15:27:29 +0800

    https to http

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const PORT = process.env.PORT || 3000;
 var app = express();
 
 // openweather only accept http request
-// app.use(function (req, res, next) {
-//     if (req.headers["x-forwarded-proto"] === "http") {
-//         next();
-//     } else { // https
-//         console.log("http://" + req.hostname + ":" + PORT + req.url)
-//         res.redirect("http://" + req.hostname + ":" + PORT + req.url);
-//     }
-// });
+app.use(function (req, res, next) {
+    if (req.headers["x-forwarded-proto"] === "http") {
+        next();
+    } else { // https
+        // console.log("http://" + req.hostname + ":" + PORT + req.url)
+        res.redirect("http://" + req.hostname + req.url);
+    }
+});
 
 app.use(express.static("public"));
 
